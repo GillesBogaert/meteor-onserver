ace3342840bdf19a33c07d0d53043ed39469603b
Gilles Bogaert <imgillesbogaert@gmail.com>
2018-02-27 21:29:49 +0100

    Bug fix

--- imports/api/rides/methods.js
+++ imports/api/rides/methods.js
@@ -40,7 +40,7 @@ Meteor.methods({
   },
   'rides.add.driver.current' : function (rideId, distance) {
     const ride = Rides.findOne(rideId);
-    Rides.update({ _id : rideId.toString()}, {$push: {"drivers.$" : {"drivers.$.driverid" : 'Bob', "drivers.$.distance" : '287'}}
+    Rides.update({ _id : rideId.toString()}, {$set: {"drivers.$" : {Test : "Test"}}
     })
 },
 'rides.add.driver.test' : function ({}) {
