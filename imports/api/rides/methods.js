89c3b295b450c6b87f2d67ed13e31f360be294d7
Gilles Bogaert <imgillesbogaert@gmail.com>
2018-02-27 18:18:21 +0100

    Auto stash before merge of "master" and "origin/master"

--- imports/api/rides/methods.js
+++ imports/api/rides/methods.js
@@ -40,7 +40,7 @@ Meteor.methods({
   },
   'rides.add.driver.current' : function ({rideId}) {
     const ride = Rides.findOne(rideId);
-    Rides.update({ _id : 'wyEFqqyjivgWdjEri'}, {
+    Rides.update({ _id : new Mongo.Collection.ObjectID(rideId)}, {
         $set: {drivers : ["Test", "Test2"]}
     })
 },
