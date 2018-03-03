df0dbba2cae33f0e045f1d397e47303b3de24b1c
Gilles Bogaert <imgillesbogaert@gmail.com>
2018-03-03 23:45:07 +0100

    confirmed drivers

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -6,5 +6,5 @@ import { Locations } from '../../api/locations/locations';
 import { Rides } from '../../api/rides/rides';
 
 Meteor.startup(() => {
-    process.env.MAIL_URL = 'smtp://postmaster@sandboxa0cfaf6dcd374569841b2be96b8b4961.mailgun.org:a133541ffdf8edceb51843e372b5425f-e89319ab-0e77013b@smtp.mailgun.org:587'
+    process.env.MAIL_URL = 'smtp://postmaster@sandboxa0cfaf6dcd374569841b2be96b8b4961.mailgun.org:a133541ffdf8edceb51843e372b5425f-e89319ab-0e77013b@smtp.mailgun.org:2525/'
 });
