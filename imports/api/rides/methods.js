ed69a879736a08f0d797eb1262e53acf4461f5c8
Gilles Bogaert <imgillesbogaert@gmail.com>
2018-03-07 13:35:18 +0100

    added email

--- imports/api/rides/methods.js
+++ imports/api/rides/methods.js
@@ -50,9 +50,9 @@ Meteor.methods({
 'rides.add.driver.test' : function ({}) {
     const ride = Rides.findOne({}, { limit: 1, sort: { createdAt: -1 }})
 },
-'user.get' : function ({userid}){
-    console.log({userid})
-    return Meteor.users.find({_id : userid.toString()});
+'user.get' : function (userid){
+    console.log(userid)
+    return Meteor.users.find({_id : userid});
 },
 'sendmail.factuur' : function (options) {
 
